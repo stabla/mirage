71016afb2fdbd1d59cf462e82e14fecada52eae3
stabla <stabla@protonmail.com>
2021-03-06 01:03:02 +0100

    Adding _importATT/_importGATT/__initGttServer

--- mirage/scenarios/mitm_test.py
+++ mirage/scenarios/mitm_test.py
@@ -1,6 +1,7 @@
 from mirage.core import scenario, interpreter, module
 from mirage.libs import io, ble, bt, utils
 from mirage.libs.ble_utils.firewall import *
+import configparser,os.path,subprocess
 
 class mitm_test(scenario.Scenario):
     # dict used to count occurences of a packet
@@ -65,13 +66,115 @@ class mitm_test(scenario.Scenario):
         GATT CENTRAL
 
     """
-    # Initialise server GATT and run it
-    def __setGattServer(self):
+    # Check if . is authorized/banned in firewall rules
+    def __firewallParser(self, rule):
         ...
 
+    # Import ATT Config file
+    def __importATT(self, filename="ATT_SLAVE_MITM"):
+        io.info("Importing ATT layer datas from "+filename+" ...")
+        attributes = []
+        config = configparser.ConfigParser()
+        config.read(filename)
+        for handle in config.sections():
+            attHandle = int(handle,16)
+            infos = config[handle]
+            attType = infos.get("type")
+            attValue = bytes.fromhex(infos.get("value") if infos.get("value") is not None else "")
+            # Filter should go there
+            # check_firewall_rules()
+            #   - has firewall a rule for ATT type?
+            if (0 == 1):
+                # put your logic there
+                ...
+            #   - has firewall a rule for ATT service?
+            if (0 == 1):
+                # put your logic there
+                ...
+            #   - has firewall a rule for banning system id?
+            if (0 == 1):
+                # put your logic there
+                ...
+            self.server.addAttribute(handle=attHandle,value=attValue,type=attType,permissions=["Read","Write"])
+
+    # Import GATT Config file 
+    def __importGATT(self, filename="GATT_SLAVE_MITM"):
+        io.info("Importing GATT layer datas from "+filename+" ...")
+        config = configparser.ConfigParser()
+        config.read(filename)
+        for element in config.sections():
+            infos=config[element]
+            if "type" in infos:
+                # Filter should go there
+                # check_firewall_rules()
+                #   - has ` `
+                if (0 == 1):
+                    # logic here
+                    ...
+                if ( 0 == 1):
+                    # logic here
+                    ...
+                if infos.get("type") == "service":
+                    startHandle = int(element,16)
+                    endHandle = int(infos.get("endhandle"),16)
+                    uuid = bytes.fromhex(infos.get("uuid"))
+                    if infos.get("servicetype") == "primary":
+                        self.server.addPrimaryService(uuid,startHandle)
+                    else:
+                        self.server.addSecondaryService(uuid,startHandle)
+                elif infos.get("type") == "characteristic":
+                    declarationHandle = int(element,16)
+                    uuid = bytes.fromhex(infos.get("uuid"))
+                    valueHandle = int(infos.get("valuehandle"),16)
+                    value = bytes.fromhex(infos.get("value"))
+                    permissions = infos.get("permissions").split(",")
+                    self.server.addCharacteristic(uuid,value,declarationHandle,valueHandle,permissions)
+                elif infos.get("type") == "descriptor":
+                    handle = int(element, 16)
+                    uuid = bytes.fromhex(infos.get("uuid"))
+                    value = bytes.fromhex(infos.get("value"))
+                    self.server.addDescriptor(uuid,value,handle)
+        
+    def __fileExists(self,filename):
+        return os.path.isfile(filename)
+
+    def __initGattServer(GATT_SLAVE_FILE, ATT_SLAVE_FILE):
+        self.server = ble.GATT_Server()
+        if ATT_SLAVE_FILE != "" and self.__fileExists(ATT_SLAVE_FILE):
+            self.importATT()
+        elif GATT_SLAVE_FILE != "" and self.__fileExists(GATT_SLAVE_FILE):
+            self.importGATT()
+        else:
+            io.info("No filename provided : empty database !")
+
+    # Initialise server GATT and run it
+    def __setGattServer(self, GATT_SLAVE_FILE, ATT_SLAVE_FILE):
+        # init server
+        self.__initGattServer(GATT_SLAVE_FILE, ATT_SLAVE_FILE)
+        io.success("GATT Firewall is running")
+
     # Export heart of the function here 
-    def __getGattSlave(self):
-        ...
+    def __getGattSlave(self, GATT_SLAVE_FILE, ATT_SLAVE_FILE):
+        # Load module
+        m = utils.loadModule('ble_discover')
+        # Set parameters
+        m["WHAT"] = "all"
+        m['INTERFACE'] = self.args['INTERFACE1']
+        m["START_HANDLE"] = "0x0001"
+        m["END_HANDLE"] = "0xFFFF"
+        m["FILTER_BY"] = ""
+        m["FILTER"] = ""
+        m['GATT_FILE']=GATT_SLAVE_FILE
+        ## open(m['GATT_FILE'], 'w').close()
+        # Execute to fill GATT_FILE
+        m.execute()
+
+        ## open(m['ATT_FILE'], 'w').close()
+        # Execute to fill ATT_FILE
+        m["WHAT"] = "attributes"
+        m['ATT_FILE']=ATT_SLAVE_FILE
+        m.execute()
+
 
     def onSlaveConnect(self, initiatorType="public"):
         # Entering the GATT Entering Cloning mode
@@ -92,29 +195,10 @@ class mitm_test(scenario.Scenario):
         # If conneced correctly, then clone the GATT Server
         if self.a2sEmitter.isConnected():
             io.success("Connected on slave : "+self.a2sReceiver.getCurrentConnection())
-            io.info("DISCOVER: Cloning GATT Server ")
-            # Load module
-            m = utils.loadModule('ble_discover')
-            # Set parameters
-            m["WHAT"] = "all"
-            m['INTERFACE'] = self.args['INTERFACE1']
-            m["START_HANDLE"] = "0x0001"
-            m["END_HANDLE"] = "0xFFFF"
-            m["FILTER_BY"] = ""
-            m["FILTER"] = ""
-            m['GATT_FILE']="GATT_SLAVE_MITM"
-            # Clean file - USELESS
-            ## open(m['GATT_FILE'], 'w').close()
-            # Execute to fill GATT_FILE
-            m.execute()
-
-            # Clean file - USELESS
-            # Execute to fill ATT_FILE
-            ## open(m['ATT_FILE'], 'w').close()
-            m["WHAT"] = "attributes"
-            m['ATT_FILE']="ATT_SLAVE_MITM"
-            m.execute()
-
+            
+            io.info("Discover: Cloning GATT Server ... ")
+            self.__getGattSlave("GATT_SLAVE_MITM", "ATT_SLAVE_MITM")
+            # self.__setGattServer("GATT_SLAVE_MITM", "ATT_SLAVE_MITM")
         else:
             io.fail("No active connections !") 
         
